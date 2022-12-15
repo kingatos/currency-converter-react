68b18d0ee7326e5d98946e7611b61efef04ad16b
Kinga Tos <toskinga@interia.pl>
2022-12-15 21:36:59 +0100

    created a new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Form from "./Form"
 import Container from "./Container";
+import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
       <Form />
+      <Footer />
     </Container>
   );
 }
---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Footer = () => (
+    <footer className="footer">
+        &copy; Copyright by Kinga 2022 All rights reserved.
+    </footer>
+);
+
+export default Footer; 
