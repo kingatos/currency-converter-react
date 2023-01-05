e0914a1c3adbb4306cc196c2bdf6b9c74f169668
Kinga Tos <toskinga@interia.pl>
2023-01-05 14:53:29 +0100

    added the white space

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -4,7 +4,7 @@ export const Result = ({ result }) => {
     if (result !== "")
     return (
         <p>
-             <strong className="form__result"> Result: {result.sourceAmount} {result.currencyFrom}= {(result.targetAmount).toFixed(2)} {result.currencyTo}</strong>
+             <strong className="form__result"> Result: {result.sourceAmount} {result.currencyFrom} = {(result.targetAmount).toFixed(2)} {result.currencyTo}</strong>
         </p>
     )
 };
