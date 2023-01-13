f4be6031a96817dab9a23b199908810fe3d3ee2f
Kinga Tos <toskinga@interia.pl>
2023-01-13 12:49:45 +0100

    added the new component Clock

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Form from "./Form"
 import Container from "./Container";
 import Footer from "./Footer";
+import Clock from "./Clock";
 
 function App() {
   return (
     <Container>
+      <Clock />
       <Form />
       <Footer />
     </Container>
---
+++ src/Clock/index.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import "./style.css";
+
+
+const Clock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return () =>
+            clearInterval(intervalID);
+    }, []);
+    return (
+        <div>
+            <p className="clock">
+                Today is {time.toLocaleDateString("en-US", {weekday:"long", day:"numeric", month:"long", year:"numeric"})}, 
+                &nbsp;{time.toLocaleTimeString()}
+            </p>
+        </div>
+    );
+};
+
+export default Clock;
