ae17cb54cd95fad2e9bbae9bca798b26479c63b6
Kinga Tos <toskinga@interia.pl>
2023-01-17 20:03:53 +0100

    removed the components from App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Form from "./Form"
 import Container from "./Container";
-import Footer from "./Footer";
-import Clock from "./Clock";
 
 function App() {
   return (
     <Container>
-      <Clock />
       <Form />
-      <Footer />
     </Container>
   );
 }
