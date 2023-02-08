f8f566f5cca32904b50a87c71b1f57ab9a0a53c4
Kinga Tos <toskinga@interia.pl>
2023-02-08 21:29:00 +0100

    deleted the unused files

--- src/Container/index.js
+++
@@ -1,9 +0,0 @@
-import { StyledMain } from "./styled.js";
-
-const Container = ({ children }) => (
-    <StyledMain>
-        {children}
-    </StyledMain>
-);
-
-export default Container;
--- src/Container/styled.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledMain = styled.div`
-    margin: auto;
-    max-width: 500px;
-    padding: 100px 0;
-`;
