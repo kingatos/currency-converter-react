fb0c1fdc3f1afa56d70baef3a506bcdaafe6a6a5
Kinga Tos <toskinga@interia.pl>
2023-02-25 12:50:30 +0100

    changed date to the EN from PL, cause the website is in English

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,13 +1,13 @@
 import { useCurrentDate } from "./useCurrentDate.js";
 import { Wrapper } from "./styled.js";
 
-const formattedDate = (date) => date.toLocaleDateString(undefined, {
+const formattedDate = (date) => date.toLocaleDateString("EN-US", {
     weekday: "long",
     day: "numeric",
     month: "long"
 });
 
-const formattedTime = (date) => date.toLocaleTimeString("pl");
+const formattedTime = (date) => date.toLocaleTimeString("EN-US");
 
 export const Clock = () => {
     const date = useCurrentDate();
