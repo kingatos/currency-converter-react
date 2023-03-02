12f57d1f6fbce6a3a3a4e32b35d5d34a29a71d28
Kinga Tos <toskinga@interia.pl>
2023-03-02 19:24:23 +0100

    created new component Failure

---
+++ src/Form/Failure/index.js
@@ -0,0 +1,10 @@
+import  { StyledFailure } from "./styled.js";
+
+export const Failure = () => {
+  return ( 
+  <StyledFailure>
+    Something went wrong. Please try again later.
+  </StyledFailure>
+  );
+};
+
---
+++ src/Form/Failure/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledFailure = styled.p`
+  color: ${({ theme }) => theme.color.red};
+  padding: 10px;
+`;
