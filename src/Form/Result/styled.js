dcf7f0def1021eabb6b194afe615524cee1da6eb
Kinga Tos <toskinga@interia.pl>
2023-09-19 23:22:35 +0200

    added more padding to result

--- src/Form/Result/styled.js
+++ src/Form/Result/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledResult = styled.strong`
-  padding: 5px 10px;
+  padding: 6px 10px;
   background-color: ${({ theme }) => theme.color.coral};
   border-radius: 5px;
   word-break: break-word;
