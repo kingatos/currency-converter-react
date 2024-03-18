4d8cb67fd0431e403ac6b3435bc9a2c1a7f0f8a7
Kinga Tos <toskinga@interia.pl>
2024-03-18 20:11:26 +0100

    formatted code

--- src/Form/useAPIdata.js
+++ src/Form/useAPIdata.js
@@ -8,7 +8,9 @@ export const useAPIdata = () => {
   useEffect(() => {
     const fetchExchangeRates = async () => {
       try {
-        const response = await fetch("http://api.exchangeratesapi.io/v1/latest?access_key=aab5e1520ee550cc33070b4c008ee32e");
+        const response = await fetch(
+          "http://api.exchangeratesapi.io/v1/latest?access_key=aab5e1520ee550cc33070b4c008ee32e"
+        );
 
         if (!response.ok) {
           throw new Error(response.statusText);
